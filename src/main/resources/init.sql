518e232825190305a961fcacd1a51314bf401d37
242396 Górski Jakub <242396@edu.p.lodz.pl>
2024-04-10 19:08:17 +0200

    feat: added permissions on tables for database users

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -2,6 +2,7 @@ GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.users TO ssbd02mok;
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.administrators TO ssbd02mok;
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.tenants TO ssbd02mok;
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.owners TO ssbd02mok;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.access_levels TO ssbd02mok;
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.locals TO ssbd02mol;
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.addresses TO ssbd02mol;
@@ -11,5 +12,11 @@ GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.payments TO ssbd02mol;
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.rents TO ssbd02mol;
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.role_requests TO ssbd02mol;
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.variable_fees TO ssbd02mol;
+
+GRANT SELECT ON TABLE public.users TO ssbd02auth;
+
+GRANT SELECT ON TABLE public.tenants TO ssbd02mol;
+GRANT SELECT ON TABLE public.owners TO ssbd02mol;
+GRANT SELECT ON TABLE public.access_levels TO ssbd02mol;
 
 INSERT INTO public.users (id, login, password, blocked, verified, login_attempts, version) VALUES ('d42d399a-59cd-4895-a48c-4a3b2a9e46d1', 'login', '$2a$12$bOPVAvWOC2f9gJoF37IeE.N9Ij15GfWeVlvHzDPTOJk66NimJMJ4.', false, true, 0, 1);
