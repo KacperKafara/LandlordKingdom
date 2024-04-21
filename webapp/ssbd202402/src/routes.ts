8593059ed9c2c06cd3655671c204f0b9547dcdb8
242374 Czerwonka Adam <242374@edu.p.lodz.pl>
2024-04-21 18:24:33 +0200

    feat: add login form

--- webapp/ssbd202402/src/routes.ts
+++ webapp/ssbd202402/src/routes.ts
@@ -4,13 +4,17 @@ import OwnerLayout from "./Layouts/OwnerLayout";
 import TenantLayout from "./Layouts/TenantLayout";
 import AccountLayout from "./Layouts/AccountLayout";
 import AdminTestPage from "./pages/AdminTest";
+import LoginPage from "./pages/Login";
+import PATHS from "./paths";
 
 const AdminRoutes: RouteObject[] = [{ path: "test", Component: AdminTestPage }];
 const OwnerRoutes: RouteObject[] = [];
 const TenantRoutes: RouteObject[] = [];
 const AccountRoutes: RouteObject[] = [];
 
-export const UnprotectedRoutes: RouteObject[] = [];
+export const UnprotectedRoutes: RouteObject[] = [
+  { path: PATHS.login, Component: LoginPage },
+];
 
 export const ProtectedRoutes: RouteObject[] = [
   { path: "admin", Component: AdminLayout, children: AdminRoutes },
