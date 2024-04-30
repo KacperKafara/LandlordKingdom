fb81c61c35ef98b89fb482ee9f005adb6e9aeb4f
Konrad Koza 242436 <242436@edu.p.lodz.pl>
2024-04-30 12:42:44 +0200

    fix: i18n for button

--- webapp/ssbd202402/src/pages/Me/index.tsx
+++ webapp/ssbd202402/src/pages/Me/index.tsx
@@ -1,16 +1,17 @@
 import {FC} from "react";
 import { Button } from "@/components/ui/button"
 import {Link} from "react-router-dom";
+import {useTranslation} from "react-i18next";
 
 
 const MePage : FC = () => {
-
+    const {t} = useTranslation();
 
     return (
         <>
             <div className="flex justify-center mt-5">
                 <Button variant="outline">
-                    <Link to={"info"}>Account Info</Link>
+                    <Link to={"info"}>{t("mePage.accountInfo")} </Link>
                 </Button>
             </div>
         </>
