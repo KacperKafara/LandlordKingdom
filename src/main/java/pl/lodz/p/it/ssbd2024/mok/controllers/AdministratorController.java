f89e6b0817b6171bef84ff359c83ca0766e5403e
242396 Górski Jakub <242396@edu.p.lodz.pl>
2024-05-08 17:11:50 +0200

    feat: email message for changes in access level

--- src/main/java/pl/lodz/p/it/ssbd2024/mok/controllers/AdministratorController.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/mok/controllers/AdministratorController.java
@@ -1,12 +1,10 @@
 package pl.lodz.p.it.ssbd2024.mok.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import pl.lodz.p.it.ssbd2024.exceptions.AccessLevelAlreadyRemovedException;
 import pl.lodz.p.it.ssbd2024.exceptions.NotFoundException;
 import pl.lodz.p.it.ssbd2024.model.Administrator;
 import pl.lodz.p.it.ssbd2024.mok.services.AdministratorService;
@@ -20,24 +18,26 @@ public class AdministratorController {
 
     private final AdministratorService administratorService;
 
-    @PutMapping(path = "/{id}/role")
+    @PutMapping(path = "/{id}/remove-role")
     @PreAuthorize("hasRole('ADMINISTRATOR')")
     public ResponseEntity<?> removeAccessLevel(@PathVariable UUID id) {
-        Administrator administrator;
         try {
-            administrator = administratorService.removeAdministratorAccessLevel(id);
-        } catch (AccessLevelAlreadyRemovedException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            administratorService.removeAdministratorAccessLevel(id);
         } catch (NotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
+
         return ResponseEntity.ok().build();
     }
 
     @PutMapping(path = "/{id}/add-role")
     @PreAuthorize("hasRole('ADMINISTRATOR')")
-    public ResponseEntity<?> addAccessLevel(@PathVariable UUID id) throws NotFoundException {
-        Administrator administrator = administratorService.addAdministratorAccessLevel(id);
+    public ResponseEntity<?> addAccessLevel(@PathVariable UUID id) {
+        try {
+            administratorService.addAdministratorAccessLevel(id);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
 
         return ResponseEntity.ok().build();
     }
