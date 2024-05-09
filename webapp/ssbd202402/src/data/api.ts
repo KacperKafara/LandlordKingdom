50af23e9782599f72be36f688eed89a17e7f28ed
Konrad Koza 242436 <242436@edu.p.lodz.pl>
2024-05-09 20:05:18 +0200

    fix: fixed login page

--- webapp/ssbd202402/src/data/api.ts
+++ webapp/ssbd202402/src/data/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { AxiosError } from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:8080/ssbd02/",
   timeout: 1000,
@@ -8,21 +9,28 @@ export const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-      const token = localStorage.getItem("token");
-      if (token && !config.url?.includes("auth")) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
+  const token = localStorage.getItem("token");
+  if (token && !config.url?.includes("auth")) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-      return config;
-    },
-    (error) => {
-      Promise.reject(error);
-    }
+  return config;
+},
+  (error) => {
+    Promise.reject(error);
+  }
 );
 
 api.interceptors.response.use((response) => response, (error) => {
+<<<<<<< Updated upstream
   if (error.response.status === 401) {
     // handle unauthorized
+=======
+  error = error as AxiosError;
+  if (error.response?.status === 401) {
+    //rozwiązanie tymczasowe
+    localStorage.removeItem("token");
+>>>>>>> Stashed changes
   }
   return Promise.reject(error);
 });
