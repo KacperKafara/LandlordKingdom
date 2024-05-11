7878f81031e7c2ded79177ee8b4416ef6c9d017d
Kacper Kafara 242412 <242412@edu.p.lodz.pl>
2024-05-11 22:02:07 +0200

    feat: english translations

--- webapp/ssbd202402/src/i18n/index.ts
+++ webapp/ssbd202402/src/i18n/index.ts
@@ -1,16 +1,25 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import pl from "./pl/common";
+import en from "./en/common";
 
 export const defaultNS = "common";
+let userLang = navigator.language;
+
+if (userLang !== "pl") {
+  userLang = "en";
+}
 
 i18next.use(initReactI18next).init({
   debug: true,
-  fallbackLng: "pl",
+  fallbackLng: userLang,
   defaultNS,
   resources: {
     pl: {
       common: pl,
+    },
+    en: {
+      common: en,
     },
   },
 });
