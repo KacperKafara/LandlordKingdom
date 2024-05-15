578b64381b280dceec4a37809ecabaf92362ed20
Konrad Koza 242436 <242436@edu.p.lodz.pl>
2024-05-15 18:49:44 +0200

    feat: unit tests for UserServiceTest using mocks

--- src/test/java/pl/lodz/p/it/ssbd2024/unit/MockConfig.java
+++ src/test/java/pl/lodz/p/it/ssbd2024/unit/MockConfig.java
@@ -1,17 +1,31 @@
 package pl.lodz.p.it.ssbd2024.unit;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.Email;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 import pl.lodz.p.it.ssbd2024.mok.repositories.*;
 import pl.lodz.p.it.ssbd2024.mok.services.VerificationTokenService;
 import pl.lodz.p.it.ssbd2024.mok.services.impl.JwtService;
 import pl.lodz.p.it.ssbd2024.services.EmailService;
+import pl.lodz.p.it.ssbd2024.util.Encoders;
+import pl.lodz.p.it.ssbd2024.util.SignVerifier;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @Profile("unit")
@@ -76,14 +90,24 @@ public class MockConfig {
         return Mockito.mock(PasswordVerificationTokenRepository.class);
     }
 
-    @Bean
-    VerificationTokenService verificationTokenService() {
-        return Mockito.mock(VerificationTokenService.class);
-    }
+//    @Bean
+//    VerificationTokenService verificationTokenService() {
+//        return Mockito.mock(VerificationTokenService.class);
+//    }
 
     @Bean
     OTPTokenRepository otpTokenRepository() {
         return Mockito.mock(OTPTokenRepository.class);
+    }
+
+    @Bean
+    SignVerifier signVerifier() {
+        return Mockito.mock(SignVerifier.class);
+    }
+
+    @Bean
+    Encoders encoders() {
+        return Mockito.mock(Encoders.class);
     }
 
     //TODO: mol repositories
