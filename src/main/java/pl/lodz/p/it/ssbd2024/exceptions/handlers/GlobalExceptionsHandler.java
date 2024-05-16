c84f736d881bcb8ffc3275e4f21f6776845e93c9
242352 Badek Marcel <242352@edu.p.lodz.pl>
2024-05-16 22:01:49 +0200

    feat: add global exception handlers for unsupported media type and exception

--- src/main/java/pl/lodz/p/it/ssbd2024/exceptions/handlers/GlobalExceptionsHandler.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/exceptions/handlers/GlobalExceptionsHandler.java
@@ -5,12 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.jwt.JwtValidationException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.thymeleaf.exceptions.TemplateInputException;
+import pl.lodz.p.it.ssbd2024.messages.ExceptionMessages;
 
 
 @ControllerAdvice(annotations = RestController.class)
@@ -22,26 +24,36 @@ public class GlobalExceptionsHandler {
         for (FieldError error: e.getFieldErrors()) {
             sb.append(error.getDefaultMessage()).append(", ");
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("not valid due to validation error(s): " + sb);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ExceptionMessages.VALIDATION_ERROR + sb);
     }
 
     @ExceptionHandler(TemplateInputException.class)
     ResponseEntity<String> handleEmailTemplateException(TemplateInputException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Problem occurred while trying to send email");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ExceptionMessages.TEMPLATE_ERROR);
     }
 
     @ExceptionHandler(GenericJDBCException.class)
     ResponseEntity<String> handleJDBCException(GenericJDBCException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Problem occurred while trying to connect to the database");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ExceptionMessages.JDBC_ERROR);
     }
 
     @ExceptionHandler(JwtValidationException.class)
     ResponseEntity<String> handleJwtValidationException(JwtValidationException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ExceptionMessages.INVALID_TOKEN);
     }
 
     @ExceptionHandler(ResponseStatusException.class)
     ResponseEntity<String> handleResponseStatusException(ResponseStatusException e) {
         return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    ResponseEntity<String> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(ExceptionMessages.MEDIA_NOT_SUPPORTED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<?> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ExceptionMessages.UNCAUGHT);
     }
 }
