bc0bd613b236c38dcd7c6bcbb736a179e400a67c
Adam  Czerwonka <adamczerwonka59@gmail.com>
2024-05-16 11:20:40 +0200

    fix: after merge

--- src/main/java/pl/lodz/p/it/ssbd2024/mok/dto/UserCreateRequest.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/mok/dto/UserCreateRequest.java
@@ -3,9 +3,12 @@ package pl.lodz.p.it.ssbd2024.mok.dto;
 import jakarta.validation.constraints.*;
 
 public record UserCreateRequest(
-                @NotBlank String login,
-                @NotBlank @Email String email,
-                @NotBlank @Size(min = 1, max = 50) String firstName,
-                @NotBlank @Size(min = 1, max = 50) String lastName,
-                @NotBlank @Size(min = 8) String password) {
+        @NotBlank String login,
+        @NotBlank @Email String email,
+        @NotBlank @Size(min = 1, max = 50) String firstName,
+        @NotBlank @Size(min = 1, max = 50) String lastName,
+        @NotBlank @Size(min = 8) String password,
+        @NotBlank(message = "Language name cannot be blank.")
+        @Pattern(regexp = "^(en|pl)$", message = "Language name must be 'en' or 'pl'.")
+        String language) {
 }
