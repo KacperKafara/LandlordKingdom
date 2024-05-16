18934125187ff51a94d8e9e94ff5bcd0091db41b
Kacper Kafara 242412 <242412@edu.p.lodz.pl>
2024-05-16 03:13:19 +0200

    Me controller test

--- src/main/java/pl/lodz/p/it/ssbd2024/util/SignVerifier.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/util/SignVerifier.java
@@ -4,6 +4,9 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSObject;
 import com.nimbusds.jose.JWSVerifier;
 import com.nimbusds.jose.crypto.MACVerifier;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -11,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import pl.lodz.p.it.ssbd2024.messages.OptimisticLockExceptionMessages;
 
 import java.text.ParseException;
+import java.util.Base64;
 import java.util.Map;
 import java.util.UUID;
 
@@ -19,6 +23,11 @@ public class SignVerifier {
 
     @Value("${jws.secret}")
     String secretValue;
+
+    @PostConstruct
+    public void init() {
+        secretValue = Base64.getEncoder().encodeToString(secretValue.getBytes());
+    }
 
     private boolean verifySignature(String token) throws ParseException, JOSEException {
         JWSObject jwsObject = JWSObject.parse(token);
