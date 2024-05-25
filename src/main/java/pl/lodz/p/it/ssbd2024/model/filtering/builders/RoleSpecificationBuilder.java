fbf9b54dfa67ed9290f0cfaf3966f54ccd07999f
Konrad Koza 242436 <242436@edu.p.lodz.pl>
2024-05-25 13:58:24 +0200

    fix: filtering classses refactoring

--- src/main/java/pl/lodz/p/it/ssbd2024/model/filtering/builders/RoleSpecificationBuilder.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/model/filtering/builders/RoleSpecificationBuilder.java
@@ -1,2 +1,55 @@
-package pl.lodz.p.it.ssbd2024.model.filtering.builders;public class RoleSpecificationBuilder {
+package pl.lodz.p.it.ssbd2024.model.filtering.builders;
+
+import org.springframework.data.jpa.domain.Specification;
+import pl.lodz.p.it.ssbd2024.exceptions.InvalidDataException;
+import pl.lodz.p.it.ssbd2024.messages.FilterMessages;
+import pl.lodz.p.it.ssbd2024.model.filtering.SearchCriteria;
+import pl.lodz.p.it.ssbd2024.model.filtering.SearchOperation;
+import pl.lodz.p.it.ssbd2024.model.filtering.specifications.RoleSpecification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoleSpecificationBuilder<T> implements SpecificationBuilder<T> {
+    private final List<SearchCriteria> params;
+
+    public RoleSpecificationBuilder() {
+        params = new ArrayList<>();
+    }
+
+    public final RoleSpecificationBuilder<T> with(String key, String operation, Object value) {
+        params.add(new SearchCriteria(key, operation, value));
+        return this;
+    }
+
+    public final RoleSpecificationBuilder<T> with(SearchCriteria searchCriteria) {
+        params.add(searchCriteria);
+        return this;
+    }
+
+    @Override
+    public SpecificationBuilder<T> with(List<SearchCriteria> searchCriteria) {
+        params.addAll(searchCriteria);
+        return this;
+    }
+
+    public Specification<T> build() throws InvalidDataException {
+        try {
+            if (params.isEmpty()) {
+                return null;
+            }
+
+            Specification<T> result = new RoleSpecification<>(params.getFirst());
+            for (int i = 1; i < params.size(); i++) {
+                SearchCriteria criteria = params.get(i);
+                result = SearchOperation.getDataOption(criteria.getDataOption()).equals(SearchOperation.ALL) ?
+                        Specification.where(result).and(new RoleSpecification<>(criteria)) :
+                        Specification.where(result).or(new RoleSpecification<>(criteria));
+            }
+
+            return result;
+        } catch (Exception e) {
+            throw new InvalidDataException(FilterMessages.INVALID_DATA);
+        }
+    }
 }
--- src/main/java/pl/lodz/p/it/ssbd2024/model/filtering/builders/SpecificationBuilder.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/model/filtering/builders/SpecificationBuilder.java
@@ -1,9 +1,9 @@
-package pl.lodz.p.it.ssbd2024.model.filtering;
+package pl.lodz.p.it.ssbd2024.model.filtering.builders;
 
 import org.springframework.data.jpa.domain.Specification;
 import pl.lodz.p.it.ssbd2024.exceptions.InvalidDataException;
+import pl.lodz.p.it.ssbd2024.model.filtering.SearchCriteria;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface SpecificationBuilder<T> {
