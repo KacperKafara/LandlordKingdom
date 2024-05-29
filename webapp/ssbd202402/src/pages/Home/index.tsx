76907adbacb363a1626667f22cdacb2dafbb57e1
242374 Czerwonka Adam <242374@edu.p.lodz.pl>
2024-05-29 14:22:27 +0200

    fix: add correct landing page colors

--- webapp/ssbd202402/src/pages/Home/index.tsx
+++ webapp/ssbd202402/src/pages/Home/index.tsx
@@ -18,27 +18,27 @@ const HomePage: FC = () => {
   return (
     <div className="relative flex min-h-screen w-screen flex-col items-center justify-center">
       <LanguageSelector />
-      <div className="w-fit self-end absolute right-14 top-1">
+      <div className="absolute right-14 top-1 w-fit self-end">
         <ModeToggle />
       </div>
       <div className="flex flex-row items-center">
         <img
           src={homeImage}
-          className="hidden w-[400px] rounded-md shadow-2xl sm:block lg:w-[600px]"
+          className="hidden w-[400px] rounded-md shadow-2xl shadow-shadowColor sm:block lg:w-[600px]"
         />
-        <div className="flex aspect-square w-[300px] flex-col items-center justify-center gap-2 rounded-md p-10 shadow-2xl shadow-shadowColor  sm:-translate-x-10 lg:w-[500px]">
+        <div className="flex aspect-square w-[300px] flex-col items-center justify-center gap-2 rounded-md bg-card p-10 shadow-2xl shadow-shadowColor sm:-translate-x-10 lg:w-[500px]">
           <h1 className="text-center text-4xl font-bold">
             {t("logoPlaceholder")}
           </h1>
           <p className="mb-10 text-center text-lg">
             {t("homePage.manageProperties")}
           </p>
-          <Button asChild>
-            <NavLink to="/login">{t("homePage.signIn")}</NavLink>
-          </Button>
-          {t("homePage.or")}
           <Button variant="outline" asChild>
             <NavLink to="/register">{t("homePage.signUp")}</NavLink>
+          </Button>
+          {t("homePage.or")}
+          <Button asChild>
+            <NavLink to="/login">{t("homePage.signIn")}</NavLink>
           </Button>
         </div>
       </div>
