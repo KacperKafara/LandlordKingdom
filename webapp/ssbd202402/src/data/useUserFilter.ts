5ed8c728b41b0bee58409613ed10fca5b9100d5a
242352 Badek Marcel <242352@edu.p.lodz.pl>
2024-05-31 00:49:48 +0200

    feat: correct email subject and error display

--- webapp/ssbd202402/src/data/useUserFilter.ts
+++ webapp/ssbd202402/src/data/useUserFilter.ts
@@ -1,9 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPrivate from "./useAxiosPrivate";
-import {AxiosError} from "axios";
-import {toast} from "@/components/ui/use-toast.ts";
-import {t} from "i18next";
-import {ErrorCode} from "@/@types/errorCode.ts";
 
 type UserFilterResponse = {
   login: string;
@@ -21,20 +17,8 @@ export const useUserFilter = () => {
   const { data } = useQuery({
     queryKey: ["userFilter"],
     queryFn: async () => {
-      try {
-        const { data } = await api.get<UserFilterResponse>("/filter/user");
-        return data;
-      } catch (error) {
-        const axiosError = error as AxiosError;
-        toast({
-          variant: "destructive",
-          title: t("userDataPage.error"),
-          description: t(
-              `errors.${(axiosError.response?.data as ErrorCode).exceptionCode}`
-          ),
-        });
-        return Promise.reject(error);
-      }
+      const { data } = await api.get<UserFilterResponse>("/filter/user");
+      return data;
     },
   });
 
