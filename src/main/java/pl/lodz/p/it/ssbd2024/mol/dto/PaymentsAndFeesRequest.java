fbc34d14da4e16a51b94685f9a739dd1a37a1fd5
242374 Czerwonka Adam <242374@edu.p.lodz.pl>
2024-06-11 21:27:57 +0200

    fix: after merge cleanup

--- src/main/java/pl/lodz/p/it/ssbd2024/mol/dto/PaymentsAndFeesRequest.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/mol/dto/PaymentsAndFeesRequest.java
@@ -1,10 +1,11 @@
 package pl.lodz.p.it.ssbd2024.mol.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import pl.lodz.p.it.ssbd2024.util.ValidDate;
+
 public record PaymentsAndFeesRequest(
         @NotBlank
         @ValidDate
-import jakarta.validation.constraints.NotBlank;
-import pl.lodz.p.it.ssbd2024.util.ValidDate;
         String startDate,
 
         @NotBlank
