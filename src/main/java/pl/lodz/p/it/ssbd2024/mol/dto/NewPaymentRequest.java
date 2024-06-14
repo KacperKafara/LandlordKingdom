c3b066182e147f34c46ac0cf09f80f3eec0e6a10
Jakub Górski <242396@edu.p.lodz.pl>
2024-06-14 03:42:26 +0200

    fix: adjusted validation to be consistent with other monetary limits

--- src/main/java/pl/lodz/p/it/ssbd2024/mol/dto/NewPaymentRequest.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/mol/dto/NewPaymentRequest.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 public record NewPaymentRequest(
         @NotNull(message = "Payment cannot be null.")
         @DecimalMin(value = "0.0", inclusive = false, message = "Payment must be greater than 0.")
-        @DecimalMax(value = "10000", inclusive = true, message = "Payment must be less than or equal to 10 000.00")
+        @DecimalMax(value = "1000000", inclusive = true, message = "Payment must be less than or equal to 1 000 000.00")
         @Digits(integer = 10, fraction = 2, message = "Payment must be a valid monetary amount with up to 10 integer digits and 2 fractional digits.")
         BigDecimal amount) {
 }
