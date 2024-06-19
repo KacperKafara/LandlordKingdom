402aba17add7f78adef857abe08a955ab20f92ae
242374 Czerwonka Adam <242374@edu.p.lodz.pl>
2024-06-20 00:13:39 +0200

    feat: add BV on VariableFeeRequest and add missing messages SSBD202402-400

--- src/main/java/pl/lodz/p/it/ssbd2024/mol/dto/PaymentsAndFeesRequest.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/mol/dto/PaymentsAndFeesRequest.java
@@ -4,12 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import pl.lodz.p.it.ssbd2024.util.ValidDate;
 
 public record PaymentsAndFeesRequest(
-        @NotBlank
-        @ValidDate
+        @NotBlank(message = "Start date cannot be blank.")
+        @ValidDate(message = "Start date must be a valid date.")
         String startDate,
 
-        @NotBlank
-        @ValidDate
+        @NotBlank(message = "End date cannot be blank.")
+        @ValidDate(message = "End date must be a valid date.")
         String endDate
 ) {
 }
