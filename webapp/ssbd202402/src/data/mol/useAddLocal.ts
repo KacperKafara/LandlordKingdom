86929f06b961819a0865ee3e527abeeeed6a6144
Jakub Górski <242396@edu.p.lodz.pl>
2024-06-27 18:59:03 +0200

    merge: merge

--- webapp/ssbd202402/src/data/mol/useAddLocal.ts
+++ webapp/ssbd202402/src/data/mol/useAddLocal.ts
@@ -5,22 +5,26 @@ import useAxiosPrivate from "@/data/useAxiosPrivate.ts";
 import { AddLocal } from "@/types/mol/Locals.ts";
 import { toast } from "@/components/ui/use-toast.ts";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
 export const useAddLocal = () => {
   const { api } = useAxiosPrivate();
   const { t } = useTranslation();
+  const navigate = useNavigate();
 
   const { mutateAsync, isPending } = useMutation({
     mutationFn: async (data: AddLocal) => {
       const result = await api.post("/locals", data);
       return result.data;
     },
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast({
         variant: "success",
         title: t("addLocalPage.successTitle"),
         description: t("addLocalPage.successDescription"),
       });
+
+      navigate(`/owner/locals/local/${data.id}`);
     },
     onError: (error: AxiosError) => {
       toast({
