4f12eb6b09b741443755fe510561f008fb0b4d68
Kacper Kafara <kacperkafara18@gmail.com>
2024-12-01 11:05:18 +0100

    some changes

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-minikube start --extra-config=apiserver.service-node-port-range=1-65535
+minikube config set memory 8192
+minikube config set cpus 6
 
-kubectl apply -k k8s
+minikube start --extra-config=apiserver.service-node-port-range=1-65535
+minikube addons enable metrics-server
+
+helm repo add kube-logging https://kube-logging.github.io/helm-charts
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+helm install monitoring ./monitoring --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsersHelmValues=false --namespace monitoring --create-namespace -f values/prometheus-conf.yaml
+helm install --create-namespace --namespace logging logging-operator oci://ghcr.io/kube-logging/helm-charts/logging-operator --version 4.11.2-dev.1
+
+helm install landlord ./helm
 
 echo "Waiting for all pods to be in 'Running' state..."
 sleep 15
